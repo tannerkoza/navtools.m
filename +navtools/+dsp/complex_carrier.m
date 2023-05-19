1d85fe5498e091fd1920bfd94a054ed48b161b05
Tanner Koza <jtk0018@auburn.edu>
2023-05-19 15:00:47 -0500

    Add carrier freq rate and cn0 arguments

--- +navtools/+dsp/complex_carrier.m
+++ +navtools/+dsp/complex_carrier.m
@@ -1,4 +1,4 @@
-function carrier = complex_carrier(fcarrier, fsamp, duration, noise)
+function carrier = complex_carrier(fcarrier, fcarrierrate, fsamp, duration, cn0)
 %IQ_CARRIER Produces an IQ carrier signal based on the sampling frequency,
 %carrier frequency, and duration of a signal.
 %
@@ -16,11 +16,12 @@ function carrier = complex_carrier(fcarrier, fsamp, duration, noise)
 %   See also NAVTOOLS.SIGNALS.REAL_CARRIER
 
 t = 0:(1/fsamp):(duration-1/fsamp);
-carrier = exp(1i*2*pi*fcarrier*t);
+carrier = exp(1i*2*pi*(fcarrier*t + 0.5*fcarrierrate*t.^2));
 
-if exist('noise', 'var')
-    num_samps = length(t); 
-    carrier = carrier + noise*[1 1j]*randn(2,num_samps)/sqrt(2); % sqrt(2) divides noise equally amongst channels
+if exist('cn0','var')
+    num_samps = length(t);
+    A = sqrt(2*10^(cn0*1e-1)/fsamp);
+    carrier = A*carrier + [1 1j]*randn(2,num_samps)/sqrt(2); % sqrt(2) divides noise equally amongst channels
 end
 
 end
